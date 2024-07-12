7958b191cfe50b0d6e6645c19ae9465294a843fd
David Adams <dadams2212@gmail.com>
2024-07-12 12:18:38 -0700

    reference chart lookup and check solution switch

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,20 +1,35 @@
 import PreflopChart from '@/components/PreflopChart/PreflopChart';
-import { Center, Group } from '@mantine/core';
-import ConfigurationControls from '@/components/ConfigurationControls/ConfigurationControls';
-import { useState } from 'react';
-import { PreflopAction } from '@/consts';
+import {Center, Group, Select, Stack, Switch} from '@mantine/core';
+import {useState} from 'react';
+import {Position, POSITIONS, PreflopAction, RFI_CHARTS} from '@/consts';
+import PreflopActionCard from "@/components/ConfigurationControls/components/PreflopActionCard";
+import _ from "lodash";
+import {useToggle} from "@mantine/hooks";
 
 export function HomePage() {
-  const [selectedPreflopAction, setSelectedPreflopAction] = useState<PreflopAction>(
+    const[selectedPosition, setSelectedPosition] = useState<Position>(Position.BN);
+  const [selectedPreflopAction, setSelectedPreflopAction] = useState<PreflopAction | null>(
     PreflopAction.Fold
   );
+  const [checkSolution, setCheckSolution] = useState<boolean | undefined>(false);
+
+  const referenceChart = RFI_CHARTS[selectedPosition];
+  const possibleActions = _.uniq(_.flatten(referenceChart || []));
+
   return (
     <Center mih="100vh">
       <Group align="flex-start">
-        <ConfigurationControls
-          selectedPreflopAction={selectedPreflopAction}
-          onSelectPreflopAction={setSelectedPreflopAction}
-        />
+          <Stack>
+              <Select label="Position" data={POSITIONS} defaultValue="BN" onChange={setSelectedPosition} />
+              {possibleActions.map((action) => (
+                  <PreflopActionCard
+                      action={action}
+                      selected={selectedPreflopAction === action}
+                      onClick={() => setSelectedPreflopAction(action)}
+                  />
+              ))}
+              {referenceChart && <Switch label="Show solution" checked={checkSolution} onChange={(e) => setCheckSolution(e.target.checked)} />}
+          </Stack>
         <PreflopChart selectedPreflopAction={selectedPreflopAction} />
       </Group>
     </Center>
